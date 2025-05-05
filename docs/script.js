3475ce55852657bf7db73ce54782f42c593da43f
CaHeK2009 <alex.chernya20@gmail.com>
2025-05-05 17:40:53 +0300

    ну вот теперь прям точно почти все

--- docs/script.js
+++ docs/script.js
@@ -120,4 +120,4 @@ const inProgressAlert = () => {
     alert('Данный раздел находится в разработке. Не трогайте его, он боится людей')
 }
 
-window.location.pathname.split('/').pop() == '' ? displayMainButtons() : displayImages()
+window.location.pathname.split('/').pop() == 'html' ? displayImages() : displayMainButtons()
